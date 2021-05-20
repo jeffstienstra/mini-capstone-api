b29568f4ba982473902fe39e65c072c83425b174
jeffstienstra <jeffstienstra@gmail.com>
2021-05-19 20:59:18 -0400

    added belongs_to/has_many relationships

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,6 +9,7 @@ class Product < ApplicationRecord
   has_many :images
   belongs_to :supplier
   has_many :orders
+  has_many :category_products
   #belongs to supplier (uses 'find_by' b/c it only has one supplier)
   # def supplier
   #   Supplier.find_by(id: supplier_id)
